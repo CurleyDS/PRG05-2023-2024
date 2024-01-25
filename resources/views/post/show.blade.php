53d1ef0718f54248d03977eac8d2a6c0c19849ce
Dylan Schoop <42898108+CurleyDS@users.noreply.github.com>
2024-01-25 11:25:17 +0100

    small fix

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -8,7 +8,7 @@ Chirper
     <div class="border-b p-2">
         <a href="{{ url('/' . $post->user->name) }}" class="text-xl dark:text-white">{{ $post->user->name }}</a>
         <p class="text-xl dark:text-white">{{ $post->text }}</p>
-        @if ($post->user_id == auth()->user()->id)
+        @if (auth()->user()->id == 1 || $post->user_id == auth()->user()->id)
             <div class="my-2">
                 <a href="{{ url('/chirp/' . $post->id) . '/edit' }}" class="p-2 text-gray-500 bg-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">edit post</a>
                 <a href="{{ url('/chirp/' . $post->id) . '/delete' }}" class="p-2 text-gray-500 bg-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">delete post</a>
